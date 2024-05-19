23334a44ce2509a6ef3533114486ebcdfc99883d
teddio496 <teddio496@gmail.com>
2024-05-19 03:05:37 -0400

    pseudo code for the get matchids

---
+++ bfs_match_ids/get_match_ids.py
@@ -0,0 +1,12 @@
+
+
+
+def bfs_get_match_ids(amount: int, start: str, already: list[str]):
+    
+    #get recent x games of the start player
+
+    #go through each of them , save the match_ids. (checks whether it already is in 'already')
+    #then add the players of each game into the queue
+    
+    #repeat until target amount has been met
+    
